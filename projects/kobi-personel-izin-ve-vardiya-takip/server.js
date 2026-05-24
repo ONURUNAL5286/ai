1a3a177747a032e74e1a849d6f3569174bfc7427
onurunal <33096514+ONURUNAL5286@users.noreply.github.com>
2026-05-25 00:30:35 +0300

    Update project workspace for KOBI Personel Izin ve Vardiya Takip - Servis Operasyon Merkezi

--- projects/kobi-personel-izin-ve-vardiya-takip/server.js
+++ projects/kobi-personel-izin-ve-vardiya-takip/server.js
@@ -44,7 +44,7 @@ function listen(port) {
   });
 
   server.listen(port, () => {
-    console.log(`KOBI Personel Izin ve Vardiya Takip - Modern UI Guncellemesi running at http://localhost:${port}`);
+    console.log(`KOBI Personel Izin ve Vardiya Takip - Servis Operasyon Merkezi running at http://localhost:${port}`);
   });
 }
 
